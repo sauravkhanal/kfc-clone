2348424a2c0ac3a30d9ed7220b4d310fc88158df
sauravkhanal <sauravkhanal635@gmail.com>
2024-04-26 14:38:46 +0545

    add horizontal ruler

--- src/components/ProductGroup.jsx
+++ src/components/ProductGroup.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import React from "react";
+import ProductCard from "./ProductCard";
 
-function ProductGroup({itemArray, groupTitle}) {
-  return (
-    <div className='flex flex-col gap-5 px-20 py-5 bg-slate-50 items-center justify-center'>
-        <p className='font-bold text-3xl font-national'>{groupTitle}</p>
-        <div className='flex flex-wrap  justify-center'>
-            {itemArray.map((itemDetail)=> <ProductCard {...itemDetail}/>
-        )}
-        </div>
-    </div>
-  )
+function ProductGroup({ itemArray, groupTitle }) {
+	return (
+		<div className="bg-slate-100">
+			<hr className="relative top-10" />
+			<div className="flex flex-col gap-5 px-20 py-5  items-center justify-center">
+				<p className="font-bold text-4xl font-national bg-slate-100 z-10 px-5">
+					{groupTitle}
+				</p>
+				<div className="flex flex-wrap  justify-center">
+					{itemArray.map((itemDetail) => (
+						<ProductCard {...itemDetail} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default ProductGroup
+export default ProductGroup;
